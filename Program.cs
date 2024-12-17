c78b6f04fb889e5d0f01227ff2a41cdfaa659b2d
GiorgiGochitidze <113135317+GiorgiGochitidze@users.noreply.github.com>
2024-12-17 22:14:32 +0400

    Fraudulent Nums Array challange

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// Defining a string array of fraudulent numbers
+string[] fraudulentNums = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
+
+/* Looping through the fraudulentNums array and using .StartsWith()
+method checking which item of fraudulentNums array starts with letter B */
+foreach (string ids in fraudulentNums){
+    if(ids.StartsWith("B")){
+        Console.WriteLine(ids);
+    }
+}
